7d565a8d354f95be757c91e4b27d2745ece41841
vanyadeg <id1997@yandex.ru>
2017-10-04 23:25:53 +0300

    HW3: part 2 and 3 solution

---
+++ HW3/databases/fundeps/find_keys.py
@@ -0,0 +1,65 @@
+# coding=utf-8
+
+
+# Класс функциональной зависимости
+class Relation(object):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return str(self.left) + " --> " + str(self.right)
+
+
+# Считаем замыкание множества аттрибутов над множеством функциональных зависимостей
+def compute_closure(relations, base):
+    closure = base
+    closure_size = len(closure)
+    while True:
+        for relation in relations:
+            if relation.left <= closure:
+                closure = closure.union(relation.right)
+        if closure_size == len(closure):
+            break
+        closure_size = len(closure)
+    return closure
+
+
+# Находим надключи размера на один меньше среди подмножеств надключа
+def find_subkeys(superkey, relations, all_attributes):
+    subkeys = set()
+    for attribute in superkey:
+        potential_subkey = superkey.difference({attribute})  # удаляем атрибут из надключа
+        if compute_closure(relations, potential_subkey) == all_attributes:  # если в замыкании все атрибуты
+            subkeys.add(superkey.difference({attribute}))
+    return subkeys
+
+
+# Находим все ключи среди множеств аттрибутов
+def find_keys(attributes, relations):
+    full_superkey = frozenset(attributes)
+    superkeys = {full_superkey}
+    keys = set()
+    while len(superkeys) > 0:
+        new_superkeys = set()
+        for superkey in superkeys:
+            subkeys = find_subkeys(superkey, relations, full_superkey)
+            if len(subkeys) == 0:  # если среди подмножеств нет надключей
+                keys.add(superkey)
+            else:
+                new_superkeys = new_superkeys.union(subkeys)
+        superkeys = new_superkeys  # переходим к надключам размером на один меньше
+    return keys
+
+
+if __name__ == "__main__":
+    relation1 = Relation({"StudentId"}, {"StudentName"})
+    relation2 = Relation({"GroupId"}, {"GroupName"})
+    relation3 = Relation({"LecturerId"}, {"LecturerName"})
+    relation4 = Relation({"CourseId"}, {"CourseName"})
+    relation5 = Relation({"StudentId"}, {"GroupId"})
+    relation6 = Relation({"CourseId"}, {"LecturerId"})
+    relation7 = Relation({"StudentId", "GroupId", "CourseId", "LecturerId"}, {"Mark"})
+    attributes = {"StudentId", "StudentName", "GroupId", "GroupName", "LecturerId", "LecturerName", "CourseId",
+                  "CourseName", "Mark"}
+    print(find_keys(attributes, {relation1, relation2, relation3, relation4, relation5, relation6, relation7}))
---
+++ HW3/databases/fundeps/irr_set.py
@@ -0,0 +1,63 @@
+# coding=utf-8
+from fundeps.find_keys import Relation, compute_closure
+
+
+# Расщепляем правые части
+def split_right_part(relations):
+    splitted = set()
+    for relation in relations:
+        for right_elem in relation.right:
+            splitted.add(Relation(relation.left, {right_elem}))
+    return splitted
+
+
+# Удаляем атрибуты из левых частей
+def delete_left_attributes(relations):
+    return set([reduce_left(relation, relations) for relation in relations])
+
+
+# Удаляем из правила все 'лишние' атрибуты
+def reduce_left(rule, relations):
+    result_left = set()
+    if len(rule.left) == 1:
+        return rule
+    for attr in rule.left:
+        without_attr = rule.left.difference({attr})
+        prev_closure = compute_closure(relations, without_attr)  # считаем старое замыкание
+        new_relations = relations.difference({rule}).union({Relation(without_attr, rule.right)})
+        new_closure = compute_closure(new_relations, without_attr)  # считаем замыкание после замены правила
+        if prev_closure != new_closure:
+            result_left.add(attr)
+    return Relation(result_left, rule.right)
+
+
+# Удаляем ненужные правила
+def delete_rules(relations):
+    return filter(lambda x: not is_rule_removable(x, relations), relations)
+
+
+# Проверяем правило на 'ненужность'
+def is_rule_removable(rule, relations):
+    closure = compute_closure(relations.difference({rule}), rule.left)
+    return rule.right.issubset(closure)  # проверяем, что правая часть остается в замыкании после удаления
+
+
+# Вычисляем НМФЗ
+def find_irreducible(relations):
+    irreducible_set = split_right_part(relations)
+    irreducible_set = delete_left_attributes(irreducible_set)
+    irreducible_set = delete_rules(irreducible_set)
+    return irreducible_set
+
+
+if __name__ == "__main__":
+    relation1 = Relation({"StudentId"}, {"StudentName"})
+    relation2 = Relation({"GroupId"}, {"GroupName"})
+    relation3 = Relation({"LecturerId"}, {"LecturerName"})
+    relation4 = Relation({"CourseId"}, {"CourseName"})
+    relation5 = Relation({"StudentId"}, {"GroupId"})
+    relation6 = Relation({"CourseId"}, {"LecturerId"})
+    relation7 = Relation({"StudentId", "GroupId", "CourseId", "LecturerId"}, {"Mark"})
+    irr_set = find_irreducible({relation7, relation6, relation5, relation4, relation3, relation2, relation1})
+    for relation in irr_set:
+        print(relation)
